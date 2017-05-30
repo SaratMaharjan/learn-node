ea98cf165fff0c20cc4a8c30e9f58db5d75a1765
saratmaharjan <sarat.maharjan@gmail.com>
2017-05-30 14:28:07 +0200

    tests

---
+++ test/arrayTest.js
@@ -0,0 +1,13 @@
+//  Uncomment if using Node
+var { describe, it } = require('mocha');
+var chai = require('chai');
+
+var assert = chai.assert;
+
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = [];
+
+    assert.equal(arr.length, 0, 'Array length was not 0');
+  });
+});
